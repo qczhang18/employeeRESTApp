72493e2d369051d86c996b20272292a7ac5d0248
Q. Zhang <qczhang18@gmail.com>
2017-11-25 18:02:05 -0500

    add readme

--- src/main/java/com/example/rest/employeerestapp/EmployeerestappApplication.java
+++ src/main/java/com/example/rest/employeerestapp/EmployeerestappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeerestappApplication {
 
 	public static void main(String[] args) {
+        System.out.println("stuff running");
 		SpringApplication.run(EmployeerestappApplication.class, args);
 	}
 }
