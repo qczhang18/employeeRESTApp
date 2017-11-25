4bc1f69adb02339faf45d1da8dfac35496686169
Q. Zhang <qczhang18@gmail.com>
2017-11-25 18:38:36 -0500

    test comment out port

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -8,16 +8,16 @@ import java.util.Properties;
 @SpringBootApplication
 public class App {
 
-    public static final Properties myProps = new Properties();
+//    public static final Properties myProps = new Properties();
 
 	public static void main(String[] args) {
 
-	    myProps.setProperty("server.address", "localhost");
-	    myProps.setProperty("server.port", "8080");
+//	    myProps.setProperty("server.address", "localhost");
+//	    myProps.setProperty("server.port", "8080");
 
 
 	    SpringApplication app = new SpringApplication(App.class);
-	    app.setDefaultProperties(myProps);
+//	    app.setDefaultProperties(myProps);
 	    app.run(args);
 	}
 }
