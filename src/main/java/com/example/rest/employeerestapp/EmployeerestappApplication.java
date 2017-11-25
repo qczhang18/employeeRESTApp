83fb4be7e645843e3569b6bb0cf6a0a13849e87e
Q. Zhang <qczhang18@gmail.com>
2017-11-25 17:04:53 -0500

    init

---
+++ src/main/java/com/example/rest/employeerestapp/EmployeerestappApplication.java
@@ -0,0 +1,12 @@
+package com.example.rest.employeerestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeerestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeerestappApplication.class, args);
+	}
+}
